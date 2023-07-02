d94d04e05e3ed0da4b7f5abaf8f8bb51663e4343
aberrantian <aberrantian@gmail.com>
2023-07-02 20:44:55 +0100

    update GAME_BOARD object

--- main.js
+++ main.js
@@ -6,18 +6,42 @@ const GAME_BOARD = (function() {
         [' ', ' ', ' ']  // 2
     ]
 
+    function print() {
+        const output = `\
+${array[0][0]} | ${array[0][1]} | ${array[0][2]}
+---------
+${array[1][0]} | ${array[1][1]} | ${array[1][2]}
+---------
+${array[2][0]} | ${array[2][1]} | ${array[2][2]}`;
+
+        console.log(output)
+    }
+
     function set(x, y, playerID) {
         xIsInRange = x >= 0 && x <= 2;
         yIsInRange = y >= 0 && y <= 2;
+        isLegal = array[x][y] == ' ';
 
-        if (xIsInRange && yIsInRange) {
-            console.log('X and Y are in range');
-        } else {
-            console.warn(`X and Y are not in range:\nX: ${x}\tY: ${y}`);
+        if (xIsInRange == false) {
+            throw new Error(`X is out of range: ${x}`);
+        } else if (yIsInRange == false) {
+            throw new Error(`Y is out of range: ${y}`);
+        } else if (isLegal == false) {
+            throw new Error(`Input is illegal`);
         }
+
+        array[x][y] = playerID;
     }
 
-    return { array, set };
+    function clear() {
+        array = [
+                [' ', ' ', ' '],
+                [' ', ' ', ' '],
+                [' ', ' ', ' ']
+            ]
+    }
+
+    return { array, set, print, clear };
 })();
 
 const createPlayer = (name) => {
