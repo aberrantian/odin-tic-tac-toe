a742fdb8837adbbf6f64bff806efe31372b70863
aberrantian <aberrantian@gmail.com>
2023-07-24 09:15:21 +0100

    add basic event listeners

--- main.js
+++ main.js
@@ -1,1 +1,37 @@
 "use strict";
+
+/*
+GAME
+  GRID
+    array
+  MODE
+    PvP
+    PvC
+  PLAYER
+    marker
+    wins
+  COMPUTER
+    random_ai
+    minimax_ai
+  evalGame()
+    check for winners
+      increase win counter for winner
+    check for tie
+*/
+
+document.getElementById("local-pvp").addEventListener("click", () => {
+  document.getElementById("main-menu").hidden = true;
+  document.getElementById("game-board").hidden = false;
+});
+
+document.getElementById("local-pvc").addEventListener("click", () => {
+  document.getElementById("main-menu").hidden = true;
+  document.getElementById("game-board").hidden = false;
+});
+
+document.getElementById("main-menu-btn").addEventListener("click", () => {
+  document.getElementById("game-board").hidden = true;
+  document.getElementById("main-menu").hidden = false;
+});
+
+document.getElementById("local-pvp").click();
